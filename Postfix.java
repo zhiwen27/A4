af1393a26aa5f35c91dc158237015793a00d550f
Zhiwen Ji <zji@smith.edu>
2024-09-26 16:14:02 -0400

    1

--- Postfix.java
+++ Postfix.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.io.StringReader;
 import java.util.ArrayDeque;
+import java.util.Iterator;
 
 /** 
  * Class to interpret and compute the result of arithmetic expressions 
@@ -10,17 +11,51 @@ public class Postfix {
 
   /** Run short test */
   public static void main(String[] args) {
-
     if (args.length == 0) {
       // If no arguments passed, print instructions
       System.err.println("Usage:  java Postfix <expr>");
     } else {
       // Otherwise, echo what was read in for now
-      Scanner input = new Scanner(new StringReader(args[0]));
-      while (input.hasNext()) {
-        System.out.println(input.next());
+      Scanner scannerTest = new Scanner(new StringReader(args[0]));
+      while (scannerTest.hasNext()) {
+        System.out.println(scannerTest.next());
       }
     }
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+    ArrayDeque<Object> inputQueue = Tokenizer.readTokens(input);
+    ArrayDeque<Object> stack = new ArrayDeque<>();
+    Iterator<Object> iterator = inputQueue.iterator();
+    {
+      while(iterator.hasNext()){
+        Object o = iterator.next();
+        if (o instanceof Integer){
+          stack.push(o);
+        }
+        if (o instanceof String){
+          if (o.equals("+")){
+            int last = (int)stack.pop();
+            int first = (int)stack.pop();
+            stack.push(last + first);
+          }
+          else if (o.equals("*")){
+            int last = (int)stack.pop();
+            int first = (int)stack.pop();
+            stack.push(last * first);
+          }
+          else if (o.equals("-")){
+            int last = (int)stack.pop();
+            int first = (int)stack.pop();
+            stack.push(last - first);
+          }
+          else if (o.equals("/")){
+            int last = (int)stack.pop();
+            int first = (int)stack.pop();
+            stack.push(last / first);
+          }
+        }
+      }
+    }
+    System.err.println(stack.getFirst());
   }
-
 }
